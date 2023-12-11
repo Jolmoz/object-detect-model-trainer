b625c77a54a62785d4eab9708626a8a658eceeab
Jolmoz <jolmoz1996@outlook.com>
2023-12-11 03:59:26 -0500

    ui, feat: clean imports

--- src/main/java/com/jolmoz/objectdetectmodeltrainer/api/ModelManagerServerApi.java
+++ src/main/java/com/jolmoz/objectdetectmodeltrainer/api/ModelManagerServerApi.java
@@ -4,7 +4,6 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -14,7 +13,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
